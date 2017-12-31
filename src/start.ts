a2c3ff086cf5b559d4049a4b876503a731bdd328
Jeremy Moseley <jeremy@convoy.com>
2017-12-31 17:23:22 +0000

    Start express.

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,25 @@
+import * as express from 'express';
+import * as Router from 'express-promise-router';
+
+import { config } from './config'; 
+
+import { getLogger } from './logger';
+
+const LOG = getLogger('main');
+
+// Exported just for tests.
 export async function main() {
-  console.log(`Hello World`);
+  LOG.info('Starting server...');
+  const app: express.Application = express();
+
+  const router = Router();
+
+  app.use(router);
+
+  const PORT = config.get('port') as number;
+  app.listen(PORT, () => {
+    LOG.info(`Server started on port ${PORT}.`);
+  });
 }
 
 main();
