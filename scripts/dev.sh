152ab65395c322edd8b33e1c1b192805e86cd418
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-01-02 20:11:32 +0000

    Make typescript a prod dependency.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-SKIP_INSTALL=1 forever -t --watch --watchDirectory src --watchIgnore __tests__ --minUptime 1000 --spinSleepTime 1000 -c "npm start" ./
+forever -t --watch --watchDirectory src --watchIgnore __tests__ --minUptime 1000 --spinSleepTime 1000 -c "npm start" ./
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ -z $SKIP_INSTALL ]; then
-  npm install
-fi
-
 npm run compile
 
 node ./dist/start.js
