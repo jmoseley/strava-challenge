98afec2be12d7be748315f0c98b3da2f07b05a58
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-01-02 20:07:05 +0000

    Install before starting.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-forever -t --watch --watchDirectory src --watchIgnore __tests__ --minUptime 1000 --spinSleepTime 1000 -c "npm start" ./
+SKIP_INSTALL=1 forever -t --watch --watchDirectory src --watchIgnore __tests__ --minUptime 1000 --spinSleepTime 1000 -c "npm start" ./
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z $SKIP_INSTALL ]; then
+  npm install
+fi
+
 npm run compile
 
 node ./dist/start.js
