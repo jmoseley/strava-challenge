fc6f1f41ed8787956b333cf2a49d44b6c2247c64
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-01-07 09:42:30 -0800

    Fix tests.

--- src/handlers/__tests__/auth_handler-test.ts
+++ src/handlers/__tests__/auth_handler-test.ts
@@ -20,7 +20,7 @@ describe(`AuthHandler`, () => {
           user: {
             findUsers: jest.fn(),
             updateAccessToken: jest.fn(),
-            create: jest.fn(),
+            createUserWithStrava: jest.fn(),
           },
         },
       },
@@ -75,13 +75,13 @@ describe(`AuthHandler`, () => {
       describe(`without an existing user`, () => {
         beforeEach(async () => {
           req.context.daos.user.findUsers.mockReturnValue([]);
-          req.context.daos.user.create.mockReturnValue(user);
+          req.context.daos.user.createUserWithStrava.mockReturnValue(user);
 
           await authHandler.callback(req, res, next);
         });
 
         it(`creates the user`, async () => {
-          expect(req.context.daos.user.create).toBeCalledWith(
+          expect(req.context.daos.user.createUserWithStrava).toBeCalledWith(
             _.omit(user, ['id']),
           );
         });
@@ -124,7 +124,7 @@ describe(`AuthHandler`, () => {
         });
 
         it(`does not create the user`, () => {
-          expect(req.context.daos.user.create).not.toBeCalled();
+          expect(req.context.daos.user.createUserWithStrava).not.toBeCalled();
         });
 
         it(`tries to find the user`, () => {
