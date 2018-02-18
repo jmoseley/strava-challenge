4e35f4a49486f76deeede720dd03f37d81babad0
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-02-17 17:45:37 -0800

    Add linter.

--- client/main.ts
+++ client/main.ts
@@ -3,19 +3,19 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.hello.onCreated(function helloOnCreated():void {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
-  counter() {
+  counter():number {
     return Template.instance().counter.get();
   },
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
+  'click button'(event: any, instance: any): void {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
