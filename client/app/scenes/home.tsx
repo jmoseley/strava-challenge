c2a0cad8a16b2e410edecc66df7267bf98f1696b
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-02-18 10:43:05 -0800

    Integrate strava login.

--- client/app/scenes/home.tsx
+++ client/app/scenes/home.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import AccountsUIWrapper from '../components/accounts_ui_wrapper';
 
 const Home = () => {
   return (
     <div>
       <h1>Cycle Challenge</h1>
+      <AccountsUIWrapper />
     </div>
   );
 };
