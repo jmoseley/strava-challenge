48b2a731a985ceed87d6770cec0c40fddf8cc5de
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-02-18 13:57:31 -0800

    Connect component to redux store and meteor data.

--- client/app/scenes/home.tsx
+++ client/app/scenes/home.tsx
@@ -1,13 +1,42 @@
 import * as React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { connect } from 'react-redux';
 import AccountsUIWrapper from '../components/accounts_ui_wrapper';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Cycle Challenge</h1>
-      <AccountsUIWrapper />
-    </div>
-  );
-};
+const { withTracker } = require('meteor/react-meteor-data');
 
-export default Home;
+export interface DataProps {
+  currentUser: Meteor.User;
+}
+
+export interface StateProps {
+  foo: string;
+}
+
+export interface Props extends DataProps, StateProps {}
+
+class HomeScene extends React.Component<Props> {
+  render() {
+    console.log('user', this.props.currentUser);
+    console.log('foo', this.props.foo);
+    return (
+      <div>
+        <h1>Cycle Challenge</h1>
+        <AccountsUIWrapper />
+      </div>
+    );
+  }
+}
+
+// POC that the redux connection works.
+const mapStateToProps = (state: any) => ({
+  foo: 'bar',
+});
+
+export default withTracker(() => {
+  Meteor.subscribe('users');
+
+  return {
+    currentUser: Meteor.user(),
+  };
+})(connect<StateProps>(mapStateToProps)(HomeScene));
