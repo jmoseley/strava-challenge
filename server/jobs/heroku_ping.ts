aed91a9c90402f089319aa449c2d36773f9673d1
Jeremy Moseley <jeremy@jeremymoseley.net>
2018-03-02 22:07:56 -0800

    Add json endpoint for status.

---
+++ server/jobs/heroku_ping.ts
@@ -0,0 +1,21 @@
+import * as _ from 'lodash';
+import { Meteor } from 'meteor/meteor';
+import * as moment from 'moment';
+
+import { runRepeatingJob, JobResult, RunArguments } from '../lib/jobs';
+import StravaProviderDAO from '../providers/strava';
+import { Collection as ActivitiesCollection } from '../../imports/models/activities';
+
+export const SELF_PING_JOB_ID = 'selfPing';
+
+async function selfPing(_args: RunArguments): Promise<JobResult> {
+  console.info(`Running ${SELF_PING_JOB_ID}`);
+
+  return JobResult.SUCCESS;
+}
+
+runRepeatingJob({
+  name: SELF_PING_JOB_ID,
+  job: selfPing,
+  repeatMinutes: 5,
+});
